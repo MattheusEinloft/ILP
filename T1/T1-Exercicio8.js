b8d57308d90da7f43a89b3aac385f14d902b9aae
Mattheus <mattheuseinloft@gmail.com>
2019-04-09 15:42:41 -0300

    Add T1-Exercicio8.js

---
+++ T1/T1-Exercicio8.js
@@ -0,0 +1,44 @@
+class Produto {
+  constructor(id, valor, percentualAumento) {
+    this.id = Number(id);
+    this.valor = Number(valor);
+    this.percentualAumento = Number(percentualAumento);
+  }
+
+  mostrarDados() {
+    console.log(`[${this.id}] Valor: ${this.valor}; Percentual de aumento: ${this.percentualAumento}`);
+  }
+}
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function mostrarTudoArray(array) {
+  for(let produto of array) {
+    produto.mostrarDados();
+  }
+}
+
+function criarArrayProdutos(quantidade) {
+  let produtos = [];
+
+  for(let i = 1; i <= quantidade; i++) {
+    let id = i;
+    let valor = generateRandomNumber(30, 300);
+    let percentualAumento = generateRandomNumber(0, 20);
+
+    produtos.push(new Produto(id, valor, percentualAumento));
+  }
+
+  return produtos;
+}
+
+let produtos = criarArrayProdutos(5);
+mostrarTudoArray(produtos);
+
+console.log('\n');
+
+// ---------------------------------------------------------------
+
+
